d8e39c1ec4d49c5e20cb03252668c6c7100dd93a
Michael Schellenberger Costa <miscco@nvidia.com>
2025-02-24 19:01:16 +0100

    Use the right version macro `CCCL_MAJOR_VERSION` (#18073)
    I accidentally used `CCCL_VERSION_MAJOR`, which is used in our cmake scripts
    
    Authors:
      - Michael Schellenberger Costa (https://github.com/miscco)
    
    Approvers:
      - David Wendt (https://github.com/davidwendt)
      - Bradley Dice (https://github.com/bdice)
      - Yunsong Wang (https://github.com/PointKernel)
    
    URL: https://github.com/rapidsai/cudf/pull/18073

--- cpp/src/io/fst/dispatch_dfa.cuh
+++ cpp/src/io/fst/dispatch_dfa.cuh
@@ -417,7 +417,7 @@ struct DispatchFSM : DeviceFSMPolicy {
     // Alias the temporary allocations from the single storage blob (or compute the necessary size
     // of the blob)
     // TODO (@miscco): remove this once rapids moves to CCCL 2.8
-#if CCCL_VERSION_MAJOR >= 3
+#if CCCL_MAJOR_VERSION >= 3
     error = hipcub::detail::AliasTemporaries(
 #else   // ^^^ CCCL 3.x ^^^ / vvv CCCL 2.x vvv
     error = hipcub::AliasTemporaries(
