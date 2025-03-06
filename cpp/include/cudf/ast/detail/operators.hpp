f3ce4ba8d7322a3c8db52012081da3caa65dff2d
Michael Schellenberger Costa <miscco@nvidia.com>
2025-03-06 23:19:04 +0100

    More minor fixes for CCCL (#17793)
    This drops the final use of `thrust::optional` in cuDF
    
    Authors:
      - Michael Schellenberger Costa (https://github.com/miscco)
      - Vyas Ramasubramani (https://github.com/vyasr)
    
    Approvers:
      - Bradley Dice (https://github.com/bdice)
      - David Wendt (https://github.com/davidwendt)
    
    URL: https://github.com/rapidsai/cudf/pull/17793

--- cpp/include/cudf/ast/detail/operators.hpp
+++ cpp/include/cudf/ast/detail/operators.hpp
@@ -44,8 +44,8 @@
 #include <cudf/utilities/error.hpp>
 #include <cudf/utilities/type_dispatcher.hpp>
 
+#include <cuda/std/optional>
 #include <cuda/std/type_traits>
-#include <thrust/optional.h>
 
 #include <cmath>
 #include <type_traits>
@@ -58,14 +58,14 @@ namespace ast {
 
 namespace detail {
 
-// Type trait for wrapping nullable types in a thrust::optional. Non-nullable
+// Type trait for wrapping nullable types in a cuda::std::optional. Non-nullable
 // types are returned as is.
 template <typename T, bool has_nulls>
 struct possibly_null_value;
 
 template <typename T>
 struct possibly_null_value<T, true> {
-  using type = thrust::optional<T>;
+  using type = cuda::std::optional<T>;
 };
 
 template <typename T>
@@ -857,7 +857,7 @@ struct operator_functor<ast_operator::IS_NULL, true> {
   static constexpr auto arity = NonNullOperator::arity;
 
   template <typename LHS>
-  __host__ __device__ inline auto operator()(LHS const lhs) -> decltype(!lhs.has_value())
+  __host__ __device__ inline auto operator()(LHS const lhs) -> bool
   {
     return !lhs.has_value();
   }
