032f2bd0c22ffb5f557c5496d310b447600c3d14
Tom Augspurger <toaugspurger@nvidia.com>
2025-03-18 19:24:11 -0500

    Require type annotations in cudf.polars (#18285)
    This updates our mypy configuration to disallow untyped function defintions in cudf.polars. This turned up a few functions without type annotations, which I've added.
    
    Closes https://github.com/rapidsai/cudf/issues/18269
    
    Authors:
      - Tom Augspurger (https://github.com/TomAugspurger)
    
    Approvers:
      - Lawrence Mitchell (https://github.com/wence-)
      - Matthew Murray (https://github.com/Matt711)
      - Vyas Ramasubramani (https://github.com/vyasr)
    
    URL: https://github.com/rapidsai/cudf/pull/18285

--- python/cudf_polars/cudf_polars/dsl/ir.py
+++ python/cudf_polars/cudf_polars/dsl/ir.py
@@ -450,7 +450,7 @@ class Scan(IR):
         n_rows: int,
         row_index: tuple[str, int] | None,
         predicate: expr.NamedExpr | None,
-    ):
+    ) -> DataFrame:
         """Evaluate and return a dataframe."""
         if typ == "csv":
             parse_options = reader_options["parse_options"]
@@ -566,7 +566,7 @@ class Scan(IR):
                 chk = reader.read_chunk()
                 rows_left_to_skip = skip_rows
 
-                def slice_skip(tbl: plc.Table):
+                def slice_skip(tbl: plc.Table) -> plc.Table:
                     nonlocal rows_left_to_skip
                     if rows_left_to_skip > 0:
                         table_rows = tbl.num_rows()
@@ -855,7 +855,7 @@ class GroupBy(IR):
             self.agg_requests = tuple(agg_requests)
             self.agg_infos = [req.collect_agg(depth=0) for req in self.agg_requests]
 
-        def __reduce__(self):
+        def __reduce__(self) -> tuple[Any, ...]:
             """Pickle an AggInfos object."""
             return (type(self), (self.agg_requests,))
 
@@ -964,7 +964,7 @@ class GroupBy(IR):
         options: GroupbyOptions,
         agg_info_wrapper: AggInfos,
         df: DataFrame,
-    ):
+    ) -> DataFrame:
         """Evaluate and return a dataframe."""
         keys = broadcast(*(k.evaluate(df) for k in keys_in), target_length=df.num_rows)
         sorted = (
@@ -1063,7 +1063,7 @@ class ConditionalJoin(IR):
             self.predicate = predicate
             self.ast = to_ast(predicate)
 
-        def __reduce__(self):
+        def __reduce__(self) -> tuple[Any, ...]:
             """Pickle a Predicate object."""
             return (type(self), (self.predicate,))
 
@@ -1495,7 +1495,7 @@ class Distinct(IR):
         zlice: Zlice | None,
         stable: bool,  # noqa: FBT001
         df: DataFrame,
-    ):
+    ) -> DataFrame:
         """Evaluate and return a dataframe."""
         if subset is None:
             indices = list(range(df.num_columns))
