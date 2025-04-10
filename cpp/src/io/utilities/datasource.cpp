ffb782fae074aaade1fe8f079234d3cb6ccb22d3
Jigao Luo <jigao.luo@outlook.com>
2025-04-10 23:46:38 +0200

    Removing unnecessary StreamSynchronization in reading (#18279)
    This PR eliminates an unnecessary default stream synchronization from the kvikio `pread` called by the `device_read_async` function. Removing this unnecessary synchronization has the potential to be beneficial for constructing a more efficient I/O and computation pipeline.
    
    Close #18278
    
    Authors:
      - Jigao Luo (https://github.com/JigaoLuo)
      - Vukasin Milovanovic (https://github.com/vuule)
    
    Approvers:
      - Tianyu Liu (https://github.com/kingcrimsontianyu)
      - Vukasin Milovanovic (https://github.com/vuule)
    
    URL: https://github.com/rapidsai/cudf/pull/18279

--- cpp/src/io/utilities/datasource.cpp
+++ cpp/src/io/utilities/datasource.cpp
@@ -154,7 +154,17 @@ class kvikio_source : public datasource {
 
 #ifdef CUDF_HAS_KVIKIO
     auto const read_size = std::min(size, this->size() - offset);
-    return _kvikio_handle.pread(dst, read_size, offset);
+    if constexpr (std::is_same_v<HandleT, kvikio::FileHandle>) {
+      return _kvikio_handle.pread(dst,
+                                  read_size,
+                                  offset,
+                                  kvikio::defaults::task_size(),
+                                  kvikio::defaults::gds_threshold(),
+                                  false /* not to sync_default_stream */);
+    } else {
+      // HandleT is kvikio::RemoteHandle
+      return _kvikio_handle.pread(dst, read_size, offset);
+    }
 #else
     return std::future<size_t>{};
 #endif
