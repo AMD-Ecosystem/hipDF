2e68c520f746cc43091937dc8d7ff05220d7b7c0
Matthew Murray <41342305+Matt711@users.noreply.github.com>
2025-04-23 19:26:59 -0400

    Skip cuml third-party integration tests that may segfault (#18561)
    Skips cuml integration tests that may fail (eg. https://github.com/rapidsai/cudf/actions/runs/14607488227/job/40980050789?pr=18554) during execution. We will let a cuml developer follow-up and potentially unskip these tests or move them to cuml.
    CC @csadorf, @vyasr
    
    Authors:
      - Matthew Murray (https://github.com/Matt711)
    
    Approvers:
      - Vyas Ramasubramani (https://github.com/vyasr)
      - GALI PREM SAGAR (https://github.com/galipremsagar)
    
    URL: https://github.com/rapidsai/cudf/pull/18561

--- python/cudf/cudf_pandas_tests/third_party_integration_tests/tests/test_cuml.py
+++ python/cudf/cudf_pandas_tests/third_party_integration_tests/tests/test_cuml.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023-2024, NVIDIA CORPORATION.
+# Copyright (c) 2023-2025, NVIDIA CORPORATION.
 
 # MIT License
 #
@@ -84,6 +84,7 @@ def binary_classification_data():
     return df
 
 
+@pytest.mark.skip(reason="Disabled due to potential segfaults")
 def test_linear_regression():
     lr = LinearRegression(fit_intercept=True, normalize=False, algorithm="eig")
     X = pd.DataFrame()
@@ -99,6 +100,7 @@ def test_linear_regression():
     return preds.values
 
 
+@pytest.mark.skip(reason="Disabled due to potential segfaults")
 def test_logistic_regression(binary_classification_data):
     X = binary_classification_data[["feature1", "feature2"]]
     y = binary_classification_data["target"]
@@ -116,6 +118,7 @@ def test_logistic_regression(binary_classification_data):
     return accuracy
 
 
+@pytest.mark.skip(reason="Disabled due to potential segfaults")
 def test_random_forest(binary_classification_data):
     X = binary_classification_data[["feature1", "feature2"]]
     y = binary_classification_data["target"]
@@ -129,6 +132,7 @@ def test_random_forest(binary_classification_data):
     return preds.values
 
 
+@pytest.mark.skip(reason="Disabled due to potential segfaults")
 def test_clustering():
     rng = np.random.default_rng(42)
     nsamps = 300
@@ -148,6 +152,7 @@ def test_data_scaling():
     return scaled_data
 
 
+@pytest.mark.skip(reason="Disabled due to potential segfaults")
 def test_pipeline(binary_classification_data):
     X = binary_classification_data[["feature1", "feature2"]]
     y = binary_classification_data["target"]
