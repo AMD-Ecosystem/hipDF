5d9f919ac41dd59ba54743e194b68d2b599d1ac1
Muhammad Haseeb <14217455+mhaseeb123@users.noreply.github.com>
2025-05-06 04:42:51 -0700

    Implement row group pruning with stats in experimental PQ reader (#18543)
    Contributes to #17896. Part of #18011.
    
    This PR implements row group pruning with stats in the experimental Parquet reader optimized for hybrid scan queries
    
    Authors:
      - Muhammad Haseeb (https://github.com/mhaseeb123)
      - Bradley Dice (https://github.com/bdice)
    
    Approvers:
      - David Wendt (https://github.com/davidwendt)
      - Vukasin Milovanovic (https://github.com/vuule)
      - Bradley Dice (https://github.com/bdice)
    
    URL: https://github.com/rapidsai/cudf/pull/18543

--- cpp/src/io/parquet/reader_impl_helpers.cpp
+++ cpp/src/io/parquet/reader_impl_helpers.cpp
@@ -1495,17 +1495,11 @@ aggregate_reader_metadata::select_columns(
 
     // Find which of the selected paths are valid and get their schema index
     std::vector<path_info> valid_selected_paths;
-    // vector reference pushback (*use_names). If filter names passed.
-    std::vector<std::reference_wrapper<std::vector<std::string> const>> column_names{};
-
-    // NOTE(HIP/AMD): Fixes prior UB if std::optional has no value.
-    if(use_names.has_value()) {
-      column_names.push_back(use_names.value());
-    }
-    if(filter_columns_names.has_value()) {
-      column_names.push_back(filter_columns_names.value());
-    }
-
+    auto const empty_names = std::vector<std::string>{};
+    // vector reference pushback for select column and/or filter names passed. Use empty names
+    // reference if the no filter column names provided.
+    std::vector<std::reference_wrapper<std::vector<std::string> const>> const column_names{
+      *use_names, filter_columns_names.has_value() ? *filter_columns_names : empty_names};
     for (auto const& used_column_names : column_names) {
       for (auto const& selected_path : used_column_names.get()) {
         auto found_path =
