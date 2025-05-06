e6e5724b211a0631e6315c211224a9d16f82d765
Bradley Dice <bdice@bradleydice.com>
2025-05-06 09:46:19 -0500

    Skip test_large_unique_categories_repr in CI (#18666)
    Resolves https://github.com/rapidsai/cudf/issues/18665.
    
    Authors:
      - Bradley Dice (https://github.com/bdice)
    
    Approvers:
      - GALI PREM SAGAR (https://github.com/galipremsagar)
    
    URL: https://github.com/rapidsai/cudf/pull/18666

--- python/cudf/cudf/tests/test_repr.py
+++ python/cudf/cudf/tests/test_repr.py
@@ -1487,9 +1487,13 @@ def test_interval_index_repr():
     assert repr(pi) == repr(gi)
 
 
-def test_unique_categories_repr():
-    pi = pd.CategoricalIndex(range(10_000))
-    gi = cudf.CategoricalIndex(range(10_000))
+@pytest.mark.skip(reason="This test is unstable in CI")
+@pytest.mark.flaky(reruns=3, reruns_delay=5)
+def test_large_unique_categories_repr():
+    # Unfortunately, this is a long running test (takes about 1 minute)
+    # and there is no way we can reduce the time
+    pi = pd.CategoricalIndex(range(100_000_000))
+    gi = cudf.CategoricalIndex(range(100_000_000))
     expected_repr = repr(pi)
     actual_repr = repr(gi)
     assert expected_repr == actual_repr
