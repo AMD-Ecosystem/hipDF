8ff8adbd3aec3837dea7799fd24c47e06619d923
Mads R. B. Kristensen <madsbk@gmail.com>
2025-05-06 18:37:55 +0200

    Polars dataframe serialize using chunked pack (#18614)
    To reduce device memory pressure, `dask_serialize` now uses `ChunkedPack` if a staging buffer is provided.
    
    Authors:
      - Mads R. B. Kristensen (https://github.com/madsbk)
      - Richard (Rick) Zamora (https://github.com/rjzamora)
    
    Approvers:
      - Richard (Rick) Zamora (https://github.com/rjzamora)
      - Tom Augspurger (https://github.com/TomAugspurger)
      - Peter Andreas Entschev (https://github.com/pentschev)
    
    URL: https://github.com/rapidsai/cudf/pull/18614

--- python/cudf_polars/cudf_polars/experimental/dask_registers.py
+++ python/cudf_polars/cudf_polars/experimental/dask_registers.py
@@ -25,6 +25,9 @@ if TYPE_CHECKING:
     from rmm.pylibrmm.memory_resource import DeviceMemoryResource
     from rmm.pylibrmm.stream import Stream
 
+    from cudf_polars.typing import ColumnHeader, ColumnOptions, DataFrameHeader
+
+
 __all__ = ["DaskRegisterManager", "register"]
 
 
@@ -173,14 +176,6 @@ def register() -> None:
             # Copy the second frame (the gpudata in host memory) back to the gpu
             frames = frames[0], plc.gpumemoryview(rmm.DeviceBuffer.to_device(frames[1]))
             return DataFrame.deserialize(header, frames)
-
-    @dask_deserialize.register(Column)
-    def _(header: ColumnHeader, frames: tuple[memoryview, memoryview]) -> Column:
-        with log_errors():
-            assert len(frames) == 2
-            # Copy the second frame (the gpudata in host memory) back to the gpu
-            frames = frames[0], plc.gpumemoryview(rmm.DeviceBuffer.to_device(frames[1]))
-            return Column.deserialize(header, frames)
 
     @sizeof_dispatch.register(Column)
     def _(x: Column) -> int:
