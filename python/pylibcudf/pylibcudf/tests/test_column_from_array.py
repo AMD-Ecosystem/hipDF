21294d3b1dd2f491bcb17ac794b87872bbe7f71a
Matthew Murray <41342305+Matt711@users.noreply.github.com>
2025-05-09 17:03:56 -0400

    Support creating a pylibcudf Column from a general ndarray (#18744)
    Contributes to https://github.com/rapidsai/cudf/issues/15132. Follows up #18425, to address comments https://github.com/rapidsai/cudf/pull/18425#discussion_r2045760125 and https://github.com/rapidsai/cudf/pull/18425#discussion_r2081792098
    
    Authors:
      - Matthew Murray (https://github.com/Matt711)
    
    Approvers:
      - Matthew Roeschke (https://github.com/mroeschke)
    
    URL: https://github.com/rapidsai/cudf/pull/18744

--- python/pylibcudf/pylibcudf/tests/test_column_from_array.py
+++ python/pylibcudf/pylibcudf/tests/test_column_from_array.py
@@ -51,7 +51,14 @@ def shape(request):
 @pytest.fixture(params=CUPY_DTYPES, ids=repr)
 def cp_array(request, shape):
     dtype = request.param
-    return np.array([0, 1, 2, 3], dtype=dtype)
+    size = np.prod(shape)
+    if dtype == np.bool_:
+        np_arr = np.array(
+            [True, False] * ((size + 1) // 2), dtype=dtype
+        ).reshape(shape)
+    else:
+        np_arr = np.arange(size, dtype=dtype).reshape(shape)
+    return cp.asarray(np_arr), np_arr
 
 
 @pytest.fixture(params=NUMPY_DTYPES, ids=repr)
@@ -74,54 +81,38 @@ def np_array(request, shape):
     return arr
 
 
-@pytest.fixture(params=CUPY_DTYPES, ids=repr)
-def cp_1darray(request):
-    dtype = request.param
-    np_data = np.array([0, 1, 2, 3], dtype=dtype)
-    return cp.asarray(np_data), np_data
+def test_from_cupy_array(cp_array):
+    cp_arr, np_arr = cp_array
+    arrow_type = pa.from_numpy_dtype(np_arr.dtype)
+    for _ in range(len(np_arr.shape) - 1):
+        arrow_type = pa.list_(arrow_type)
+    expected = pa.array(np_arr.tolist(), type=arrow_type)
 
-
-@pytest.fixture(params=CUPY_DTYPES, ids=repr)
-def cp_2darray(request):
-    dtype = request.param
-    np_data = np.array([[0, 1, 2], [3, 4, 5]], dtype=dtype)
-    return cp.asarray(np_data), np_data
-
-
-def test_from_ndarray_cupy_1d(cp_1darray):
-    cp_arr, np_arr = cp_1darray
-    expect = pa.array(np_arr, type=pa.from_numpy_dtype(np_arr.dtype))
     got = plc.Column.from_array(cp_arr)
-    assert_column_eq(expect, got)
+    assert_column_eq(expected, got)
 
 
-def test_from_ndarray_cupy_2d(cp_2darray):
-    cp_arr, np_arr = cp_2darray
-    dtype = pa.from_numpy_dtype(np_arr.dtype)
-    expect = pa.array(np_arr.tolist(), type=pa.list_(dtype))
-    got = plc.Column.from_array(cp_arr)
-    assert_column_eq(expect, got)
+def test_from_numpy_array(np_array):
+    arr = np_array
+    arrow_type = pa.from_numpy_dtype(arr.dtype)
+    for _ in range(len(arr.shape) - 1):
+        arrow_type = pa.list_(arrow_type)
+    expected = pa.array(arr.tolist(), type=arrow_type)
+
+    got = plc.Column.from_array(arr)
+    assert_column_eq(expected, got)
 
 
-def test_from_ndarray_numpy_1d(np_1darray):
-    expect = pa.array(np_1darray, type=pa.from_numpy_dtype(np_1darray.dtype))
-    got = plc.Column.from_array(np_1darray)
-    assert_column_eq(expect, got)
+def test_non_c_contiguous_raises(cp_array):
+    cp_arr = cp_array[0]
+    if len(cp_arr.shape) == 1:
+        return
 
-
-def test_from_ndarray_numpy_2d(np_2darray):
-    dtype = pa.from_numpy_dtype(np_2darray.dtype)
-    expect = pa.array(np_2darray.tolist(), type=pa.list_(dtype))
-    got = plc.Column.from_array(np_2darray)
-    assert_column_eq(expect, got)
-
-
-def test_non_c_contiguous_raises(cp_2darray):
     with pytest.raises(
         ValueError,
         match="Data must be C-contiguous",
     ):
-        plc.Column.from_array(cp.asfortranarray(cp_2darray[0]))
+        plc.Column.from_array(cp.asfortranarray(cp_arr))
 
 
 def test_row_limit_exceed_raises():
@@ -180,3 +171,12 @@ def test_array_interface_with_data_none():
         match="Expected a data field .* the array interface.",
     ):
         plc.Column.from_array(ArrayInterfaceWithNone())
+
+
+def test_from_zero_dimensional_array():
+    arr = np.array(0)
+    with pytest.raises(
+        ValueError,
+        match="shape must be a non-empty tuple",
+    ):
+        plc.Column.from_array(arr)
