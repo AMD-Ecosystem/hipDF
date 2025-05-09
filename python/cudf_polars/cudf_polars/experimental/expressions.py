14c458ea8eb25512d3ab8fc8edf70e470f8daee2
Richard (Rick) Zamora <rzamora217@gmail.com>
2025-05-09 12:27:50 -0500

    Improve lowering of ``Distinct`` IR nodes for high-cardinality data (#18725)
    ``Distinct`` lowering does not always detect high-cardinality columns when a `unique` operation is performed within a larger `Select` IR node. This PR enables Q22 to succeed in `pdsh.py` at sf3k.
    
    Authors:
      - Richard (Rick) Zamora (https://github.com/rjzamora)
    
    Approvers:
      - Peter Andreas Entschev (https://github.com/pentschev)
      - Tom Augspurger (https://github.com/TomAugspurger)
    
    URL: https://github.com/rapidsai/cudf/pull/18725

--- python/cudf_polars/cudf_polars/experimental/expressions.py
+++ python/cudf_polars/cudf_polars/experimental/expressions.py
@@ -169,6 +169,19 @@ def _decompose_unique(
         names=names,
     )
     (column,) = columns
+
+    assert config_options.executor.name == "streaming", (
+        "'in-memory' executor not supported in '_decompose_unique'"
+    )
+
+    cardinality: float | None = None
+    if cardinality_factor := {
+        max(min(v, 1.0), 0.00001)
+        for k, v in config_options.executor.cardinality_factor.items()
+        if k in _leaf_column_names(child)
+    }:
+        cardinality = max(cardinality_factor)
+
     input_ir, partition_info = lower_distinct(
         Distinct(
             {column.name: column.dtype},
@@ -181,7 +194,9 @@ def _decompose_unique(
         input_ir,
         partition_info,
         config_options,
+        cardinality=cardinality,
     )
+
     return column, input_ir, partition_info
 
 
