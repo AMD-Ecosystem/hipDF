cfef123dcd1e3b6a91641e3c3ae61283b7f9c622
Bradley Dice <bdice@bradleydice.com>
2025-05-09 23:31:19 -0500

    Simplify Numba-CUDA MVC logic (#18724)
    This PR is an experiment to see if we can simplify our MVC detection logic.
    
    On CUDA 12 we would like to unconditionally enable `pynvjitlink`, which means we can eliminate some driver/runtime version checks that are being done in a subprocess at import time.
    
    That in turn unlocks some simplifications within the code and tests.
    
    Authors:
      - Bradley Dice (https://github.com/bdice)
      - https://github.com/brandon-b-miller
    
    Approvers:
      - https://github.com/brandon-b-miller
      - Matthew Roeschke (https://github.com/mroeschke)
    
    URL: https://github.com/rapidsai/cudf/pull/18724

--- python/cudf/cudf/tests/test_mvc.py
+++ python/cudf/cudf/tests/test_mvc.py
@@ -1,4 +1,4 @@
-# Copyright (c) 2023-2024, NVIDIA CORPORATION.
+# Copyright (c) 2023-2025, NVIDIA CORPORATION.
 
 # MIT License
 #
@@ -25,8 +25,10 @@
 import subprocess
 import sys
 
-import pytest
-from packaging import version
+TEST_SCRIPT = """
+import numba.cuda
+import cudf
+from cudf.utils._numba import _CUDFNumbaConfig, _setup_numba
 
 import cudf
 
@@ -35,36 +37,8 @@ if getattr(cudf, "__is_hip_amd_port__", False):
     pytest.skip("This test is CUDA-specific and not supported on HIP/AMD platform.", 
                 allow_module_level=True)
 
-IS_CUDA_11 = False
-IS_CUDA_12 = False
-try:
-    from ptxcompiler.patch import safe_get_versions
-except ModuleNotFoundError:
-    from cudf.utils._ptxcompiler import safe_get_versions
+_setup_numba()
 
-# do not test cuda 12 if pynvjitlink isn't present
-HAVE_PYNVJITLINK = False
-try:
-    import numba
-    import pynvjitlink  # noqa: F401
-
-    HAVE_PYNVJITLINK = version.parse(numba.__version__) >= version.parse(
-        "0.58"
-    )
-except ModuleNotFoundError:
-    pass
-
-
-versions = safe_get_versions()
-driver_version, runtime_version = versions
-
-if (11, 0) <= driver_version < (12, 0):
-    IS_CUDA_11 = True
-if (12, 0) <= driver_version < (13, 0):
-    IS_CUDA_12 = True
-
-
-TEST_BODY = """
 @numba.cuda.jit
 def test_kernel(x):
     id = numba.cuda.grid(1)
@@ -76,56 +50,10 @@ with _CUDFNumbaConfig():
     test_kernel.forall(len(s))(s)
 """
 
-CUDA_11_TEST = (
-    """
-import numba.cuda
-import cudf
-from cudf.utils._numba import _CUDFNumbaConfig, patch_numba_linker_cuda_11
 
-
-patch_numba_linker_cuda_11()
-"""
-    + TEST_BODY
-)
-
-
-CUDA_12_TEST = (
-    """
-import numba.cuda
-import cudf
-from cudf.utils._numba import _CUDFNumbaConfig
-from pynvjitlink.patch import (
-    patch_numba_linker as patch_numba_linker_pynvjitlink,
-)
-
-patch_numba_linker_pynvjitlink()
-"""
-    + TEST_BODY
-)
-
-
-@pytest.mark.parametrize(
-    "test",
-    [
-        pytest.param(
-            CUDA_11_TEST,
-            marks=pytest.mark.skipif(
-                not IS_CUDA_11,
-                reason="Minor Version Compatibility test for CUDA 11",
-            ),
-        ),
-        pytest.param(
-            CUDA_12_TEST,
-            marks=pytest.mark.skipif(
-                not IS_CUDA_12 or not HAVE_PYNVJITLINK,
-                reason="Minor Version Compatibility test for CUDA 12",
-            ),
-        ),
-    ],
-)
-def test_numba_mvc(test):
+def test_numba_mvc():
     cp = subprocess.run(
-        [sys.executable, "-c", test],
+        [sys.executable, "-c", TEST_SCRIPT],
         capture_output=True,
         cwd="/",
     )
