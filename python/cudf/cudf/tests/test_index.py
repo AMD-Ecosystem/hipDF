90c68c222b0327d77af912f9f1e0dcab03cfacbe
Matthew Murray <41342305+Matt711@users.noreply.github.com>
2025-05-15 00:53:50 -0400

    Add fast paths for `DataFrame.to_cupy` (#18801)
    Contributes to #16483 by adding fast paths to `DataFrame.to_cupy` (which is called when `DataFrame.values` is called). The PR follows up #18450 to add cython bindings for `cudf::table_to_array` to pylibcudf and plumbs those changes through cudf classic.
    
    I benchmarked the fast (True) and slow (False) when the dataframe has 1, 6, 20, and 100 columns. The fast paths use `cudf::table_to_array` if the number of columns is greater than 1 and `cp.asarray` directly if the dataframe has only one column. The slow path uses a [raw python loop + assignment](https://github.com/rapidsai/cudf/blob/3e1fd22b3cb0f3bb6f16d36dcf4a79e418985b17/python/cudf/cudf/core/frame.py#L520) to create the cupy array.
    ![image](https://github.com/user-attachments/assets/4c9edfa0-e15d-4902-b597-675cfb02343d)
    
    I used the median because the CUDA overhead of calling `cudf::table_to_array` is large (so there are outliers in the times). Here is a profile of calling `to_cupy` twice for both the slow and fast paths.
    ![Screenshot from 2025-05-13 12-23-46](https://github.com/user-attachments/assets/d84fdfa3-3696-4df8-91b6-3eb9dde65430)
    
    In the first calls, the fast path takes 7.3 ms vs 4.8 ms for the slow path. The first call to `cudf::table_to_array` is the bottleneck. But if you compare the second calls, the fast path is much faster (79 us vs 2.3ms)
    
    Authors:
      - Matthew Murray (https://github.com/Matt711)
    
    Approvers:
      - Bradley Dice (https://github.com/bdice)
      - Matthew Roeschke (https://github.com/mroeschke)
    
    URL: https://github.com/rapidsai/cudf/pull/18801

--- python/cudf/cudf/tests/test_index.py
+++ python/cudf/cudf/tests/test_index.py
@@ -3344,17 +3344,6 @@ def test_categoricalindex_from_codes(ordered, name):
     assert_eq(result, expected)
 
 
-@pytest.mark.parametrize("klass", [cudf.RangeIndex, pd.RangeIndex])
-@pytest.mark.parametrize("name_inner", [None, "a"])
-@pytest.mark.parametrize("name_outer", [None, "b"])
-def test_rangeindex_accepts_rangeindex(klass, name_inner, name_outer):
-    result = cudf.RangeIndex(klass(range(1), name=name_inner), name=name_outer)
-    expected = pd.RangeIndex(
-        pd.RangeIndex(range(1), name=name_inner), name=name_outer
-    )
-    assert_eq(result, expected)
-
-
 def test_roundtrip_index_plc_column():
     index = cudf.Index([1])
     expect = cudf.Index(index)
