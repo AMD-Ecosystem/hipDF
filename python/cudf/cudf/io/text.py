666bb796f80a3a0002a6c1fef5abd371fc60d47f
Matthew Roeschke <10647082+mroeschke@users.noreply.github.com>
2025-05-15 09:37:38 -0700

    Remove toplevel `import cudf` from cudf/io files (#18829)
    Towards https://github.com/rapidsai/cudf/issues/10820.
    
    Will help us better identify if objects needs to be moved to different locations for better import dependency chains
    
    Authors:
      - Matthew Roeschke (https://github.com/mroeschke)
    
    Approvers:
      - GALI PREM SAGAR (https://github.com/galipremsagar)
    
    URL: https://github.com/rapidsai/cudf/pull/18829

--- python/cudf/cudf/io/text.py
+++ python/cudf/cudf/io/text.py
@@ -63,4 +63,4 @@ def read_text(
         byte_range=byte_range, strip_delimiters=strip_delimiters
     )
     plc_column = plc.io.text.multibyte_split(datasource, delimiter, options)
-    return cudf.Series.from_pylibcudf(plc_column)
+    return Series.from_pylibcudf(plc_column)
