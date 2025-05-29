3a7906897bb070fa3d43048ad766fb97875787a8
Basit Ayantunde <rlamarrr@gmail.com>
2025-05-29 22:48:41 +0100

    Null-handling for Transforms (#18845)
    This merge request implements null-handling for Transforms. The implementation outputs a null row if any of its input rows are null. The output bitmask is computed in a single pass using the null-masks of the input columns before the transform function is run on the valid output rows.
    
    It also handles the scalar input edge cases:
    
    - Scalar is not null; implies it is valid across the base column's size. We also can't use its bitmask as it is a bitmask of size 1, otherwise, we'd encounter an out-of-bounds error. We exclude the bitmask from the `bitmask_and` operation.
    
    - Scalar is null; implies it is null across the base column's size. This would make all the output rows null. The scalar's bitmask is also of size 1. In this case, we output a null-mask of the base column's size, and mark all the elements as null.
    
    Follows up on https://github.com/rapidsai/cudf/issues/18023, https://github.com/rapidsai/cudf/issues/18820
    
    Authors:
      - Basit Ayantunde (https://github.com/lamarrr)
    
    Approvers:
      - David Wendt (https://github.com/davidwendt)
      - Tianyu Liu (https://github.com/kingcrimsontianyu)
      - Muhammad Haseeb (https://github.com/mhaseeb123)
      - Nghia Truong (https://github.com/ttnghia)
    
    URL: https://github.com/rapidsai/cudf/pull/18845

--- java/src/test/java/ai/rapids/cudf/ColumnVectorTest.java
+++ java/src/test/java/ai/rapids/cudf/ColumnVectorTest.java
@@ -134,10 +134,10 @@ public class ColumnVectorTest extends CudfTestBase {
 
   @Test
   void testTransformVector() {
-    try (ColumnVector cv = ColumnVector.fromBoxedInts(2,3,4);
+    try (ColumnVector cv = ColumnVector.fromBoxedInts(2, 3, null, 4);
          ColumnVector cv1 = cv.transform(llvmir, true);
          ColumnVector cv2 = cv.transform(cuda, false);
-         ColumnVector expected = ColumnVector.fromBoxedInts(2*2-2, 3*3-3, 4*4-4)) {
+         ColumnVector expected = ColumnVector.fromBoxedInts(2 * 2 - 2, 3 * 3 - 3, null, 4 * 4 - 4)) {
       assertColumnsAreEqual(expected, cv1);
       assertColumnsAreEqual(expected, cv2);
     }
