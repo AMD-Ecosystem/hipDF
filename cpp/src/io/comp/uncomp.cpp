c017bf4fb6811730c9155b6db267f80d0bc00f90
Vukasin Milovanovic <vmilovanovic@nvidia.com>
2025-05-28 21:16:24 -0700

    Update nvCOMP adapter (#18931)
    Update the nvCOMP adapter to match the API changes.
    
    No functional changes with the current version.
    
    Authors:
      - Vukasin Milovanovic (https://github.com/vuule)
    
    Approvers:
      - Nghia Truong (https://github.com/ttnghia)
      - https://github.com/nvdbaranec
    
    URL: https://github.com/rapidsai/cudf/pull/18931

--- cpp/src/io/comp/uncomp.cpp
+++ cpp/src/io/comp/uncomp.cpp
@@ -38,7 +38,6 @@
 #include "common_internal.hpp"
 #include "gpuinflate.hpp"
 #include "io/utilities/getenv_or.hpp"
-#include "io/utilities/hostdevice_vector.hpp"
 #include "io_uncomp.hpp"
 #include "nvcomp_adapter.hpp"
 #include "unbz2.hpp"  // bz2 uncompress
