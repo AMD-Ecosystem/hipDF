297d40735930220080340e692fe05ec0da766647
Alessandro Bellina <abellina@nvidia.com>
2025-06-04 08:45:05 -0500

    Temporarily revert "Refactor JNI error handling (#18983)" (#19076)
    Currently the cuDF builds for 25.08 show a failed build for spark-rapids-jni due to a conflict between the spark-rapids-jni 25.06, which is being picked up due to it being the active branch in jni, built against cuDF 25.08, which has some breaking changes introduced in the commit I am reverting here.
    
    Once we move to 25.08 consistently we'll be able to bring this work back. Please also check:
    
    https://github.com/NVIDIA/spark-rapids-jni/pull/3412
    
    Authors:
      - Alessandro Bellina (https://github.com/abellina)
    
    Approvers:
      - Nghia Truong (https://github.com/ttnghia)
    
    URL: https://github.com/rapidsai/cudf/pull/19076

--- java/src/main/native/src/CudaJni.cpp
+++ java/src/main/native/src/CudaJni.cpp
@@ -35,7 +35,6 @@
 // LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
 // OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
 // SOFTWARE.
-#include "jni_utils.hpp"
 
 #include <cudf/utilities/error.hpp>
 #include <cudf/cuda_runtime.h>
@@ -44,6 +43,8 @@
 #ifdef CUDF_JNI_ENABLE_PROFILING
 #include <cuda_profiler_api.h>
 #endif
+
+#include "jni_utils.hpp"
 
 namespace {
 
@@ -178,7 +179,7 @@ JNIEXPORT void JNICALL Java_ai_rapids_cudf_Cuda_setDevice(JNIEnv* env, jclass, j
   try {
     if (Cudf_device != cudaInvalidDeviceId && dev != Cudf_device) {
       cudf::jni::throw_java_exception(
-        env, cudf::jni::CUDF_EXCEPTION_CLASS, "Cannot change device after RMM init");
+        env, cudf::jni::CUDF_ERROR_CLASS, "Cannot change device after RMM init");
     }
     CUDF_CUDA_TRY(cudaSetDevice(dev));
   }
@@ -427,7 +428,7 @@ JNIEXPORT void JNICALL Java_ai_rapids_cudf_Cuda_profilerStart(JNIEnv* env, jclas
   CATCH_STD(env, );
 #else
   cudf::jni::throw_java_exception(
-    env, cudf::jni::CUDF_EXCEPTION_CLASS, "This library was built without CUDA profiler support.");
+    env, cudf::jni::CUDF_ERROR_CLASS, "This library was built without CUDA profiler support.");
 #endif
 }
 
@@ -440,7 +441,7 @@ JNIEXPORT void JNICALL Java_ai_rapids_cudf_Cuda_profilerStop(JNIEnv* env, jclass
   CATCH_STD(env, );
 #else
   cudf::jni::throw_java_exception(
-    env, cudf::jni::CUDF_EXCEPTION_CLASS, "This library was built without CUDA profiler support.");
+    env, cudf::jni::CUDF_ERROR_CLASS, "This library was built without CUDA profiler support.");
 #endif
 }
 
