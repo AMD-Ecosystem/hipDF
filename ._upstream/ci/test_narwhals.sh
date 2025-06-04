64916e1e8fbbb5a6b6079e5ea425b099dd1ee545
Matthew Murray <41342305+Matt711@users.noreply.github.com>
2025-06-04 09:08:02 -0400

    Disable pytest benchmark for Narwhals CI job (#19074)
    Fixes the
    [failing](https://github.com/rapidsai/cudf/actions/runs/15401210507/job/43335854069?pr=19063)
    Narwhals CI job.
    
    ---------
    
    Co-authored-by: Bradley Dice <bdice@bradleydice.com>

--- ._upstream/ci/test_narwhals.sh
+++ ._upstream/ci/test_narwhals.sh
@@ -84,13 +84,7 @@ test_to_arrow_with_nulls or \
 test_pandas_object_series \
 "
 
-# test_dtypes: With cudf.pandas loaded, to_pandas() preserves Arrow dtypes like list and struct, so pandas
-# columns aren't object anymore. The test expects object, causing a mismatch.
-TESTS_THAT_NEED_NARWHALS_FIX_FOR_CUDF_PANDAS=" \
-test_dtypes \
-"
-
-NARWHALS_DEFAULT_CONSTRUCTORS=pandas python -m pytest \
+PYTEST_DISABLE_PLUGIN_AUTOLOAD=1 NARWHALS_DEFAULT_CONSTRUCTORS=pandas python -m pytest \
     -p cudf.pandas \
     --cache-clear \
     --junitxml="${RAPIDS_TESTS_DIR}/junit-cudf-pandas-narwhals.xml" \
