ed9a5bafab4a96000427c21e5f2c09ce0e5d8941
Muhammad Haseeb <14217455+mhaseeb123@users.noreply.github.com>
2025-07-11 13:09:24 -0700

    Move the Parquet `reader_impl` class declaration out of the `parquet::detail::reader` (#19305)
    Contributes to #19308
    
    This PR moves out `parquet::reader::impl` class out of `parquet::reader` to allow subclassing (the experimental reader) from it and reusing its functions without having to duplicate code
    
    Authors:
      - Muhammad Haseeb (https://github.com/mhaseeb123)
    
    Approvers:
      - Paul Mattione (https://github.com/pmattione-nvidia)
      - Tianyu Liu (https://github.com/kingcrimsontianyu)
      - Nghia Truong (https://github.com/ttnghia)
    
    URL: https://github.com/rapidsai/cudf/pull/19305

--- cpp/src/io/parquet/reader_impl_chunking.cu
+++ cpp/src/io/parquet/reader_impl_chunking.cu
@@ -76,7 +76,7 @@ constexpr float input_limit_compression_reserve = 0.3f;
 
 }  // namespace
 
-void reader::impl::handle_chunking(read_mode mode)
+void reader_impl::handle_chunking(read_mode mode)
 {
   // if this is our first time in here, setup the first pass.
   if (!_pass_itm_data) {
@@ -117,7 +117,7 @@ void reader::impl::handle_chunking(read_mode mode)
   setup_next_subpass(mode);
 }
 
-void reader::impl::setup_next_pass(read_mode mode)
+void reader_impl::setup_next_pass(read_mode mode)
 {
   auto const num_passes = _file_itm_data.num_passes();
 
@@ -241,7 +241,7 @@ void reader::impl::setup_next_pass(read_mode mode)
   }
 }
 
-void reader::impl::setup_next_subpass(read_mode mode)
+void reader_impl::setup_next_subpass(read_mode mode)
 {
   auto& pass    = *_pass_itm_data;
   pass.subpass  = std::make_unique<subpass_intermediate_data>();
@@ -366,13 +366,11 @@ void reader::impl::setup_next_subpass(read_mode mode)
   // decompress the data pages in this subpass; also decompress the dictionary pages in this pass,
   // if this is the first subpass in the pass
   if (pass.has_compressed_data) {
-    // Empty page mask indicates all pages must be decompressed
-    auto const empty_page_mask = cudf::host_span<bool>{};
     auto [pass_data, subpass_data] =
       decompress_page_data(pass.chunks,
                            is_first_subpass ? pass.pages : host_span<PageInfo>{},
                            subpass.pages,
-                           empty_page_mask,
+                           _page_mask,
                            _stream,
                            _mr);
 
@@ -430,7 +428,7 @@ void reader::impl::setup_next_subpass(read_mode mode)
 #endif
 }
 
-void reader::impl::create_global_chunk_info()
+void reader_impl::create_global_chunk_info()
 {
   auto const num_rows         = _file_itm_data.global_num_rows;
   auto const& row_groups_info = _file_itm_data.row_groups;
@@ -533,7 +531,7 @@ void reader::impl::create_global_chunk_info()
   }
 }
 
-void reader::impl::compute_input_passes(read_mode mode)
+void reader_impl::compute_input_passes(read_mode mode)
 {
   // at this point, row_groups has already been filtered down to just the row groups we need to
   // handle optional skip_rows/num_rows parameters.
@@ -652,7 +650,7 @@ void reader::impl::compute_input_passes(read_mode mode)
   }
 }
 
-void reader::impl::compute_output_chunks_for_subpass()
+void reader_impl::compute_output_chunks_for_subpass()
 {
   auto& pass    = *_pass_itm_data;
   auto& subpass = *pass.subpass;
