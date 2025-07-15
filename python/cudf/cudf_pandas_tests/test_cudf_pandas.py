5697f26f4292154d14edb00d4f2a302dfee95a19
Matthew Murray <41342305+Matt711@users.noreply.github.com>
2025-07-15 13:36:40 -0400

    Update cudf.pandas tests to silence deprecation warnings (#19377)
    Make the nightly tests pass: https://github.com/rapidsai/cudf/actions/runs/16285297143/job/45982699441
    
    Authors:
      - Matthew Murray (https://github.com/Matt711)
    
    Approvers:
      - https://github.com/brandon-b-miller
      - GALI PREM SAGAR (https://github.com/galipremsagar)
    
    URL: https://github.com/rapidsai/cudf/pull/19377

--- python/cudf/cudf_pandas_tests/test_cudf_pandas.py
+++ python/cudf/cudf_pandas_tests/test_cudf_pandas.py
@@ -233,6 +233,9 @@ def test_index_generator():
     tm.assert_equal(pi, xi)
 
 
+@pytest.mark.filterwarnings(
+    "ignore:DataFrameGroupBy.apply operated on the grouping columns"
+)
 def test_groupby_apply_fallback(dataframe, groupby_udf):
     pdf, df = dataframe
     tm.assert_equal(
@@ -324,6 +327,9 @@ def test_df_from_series(series):
     tm.assert_frame_equal(pd.DataFrame(psr), xpd.DataFrame(sr))
 
 
+@pytest.mark.filterwarnings(
+    "ignore:Setting an item of incompatible dtype is deprecated"
+)
 def test_iloc_change_type(series):
     psr, sr = series
     psr.iloc[0] = "a"
@@ -463,7 +469,9 @@ def test_is_sparse():
     psa = pd.arrays.SparseArray([0, 0, 1, 0])
     xsa = xpd.arrays.SparseArray([0, 0, 1, 0])
 
-    assert pd.api.types.is_sparse(psa) == xpd.api.types.is_sparse(xsa)  # noqa: TID251
+    assert isinstance(psa.dtype, pd.SparseDtype) == isinstance(
+        xsa.dtype, xpd.SparseDtype
+    )
 
 
 def test_is_file_like():
@@ -501,6 +509,9 @@ def test_infer_freq():
     assert expected == got
 
 
+@pytest.mark.filterwarnings(
+    "ignore:DataFrameGroupBy.apply operated on the grouping columns"
+)
 def test_groupby_grouper_fallback(dataframe, groupby_udf):
     pdf, df = dataframe
     tm.assert_equal(
@@ -950,7 +961,7 @@ def test_namedagg_namedtuple():
     result = df.groupby("kind").agg(
         min_height=pd.NamedAgg(column="height", aggfunc="min"),
         max_height=pd.NamedAgg(column="height", aggfunc="max"),
-        average_weight=pd.NamedAgg(column="weight", aggfunc=np.mean),
+        average_weight=pd.NamedAgg(column="weight", aggfunc="mean"),
     )
     expected = xpd.DataFrame(
         {
@@ -1199,6 +1210,9 @@ def test_index_new():
     tm.assert_equal(expected, got)
 
 
+@pytest.mark.filterwarnings(
+    "ignore:DataFrameGroupBy.apply operated on the grouping columns"
+)
 @pytest.mark.xfail(not LOADED, reason="Should not fail in accelerated mode")
 def test_groupby_apply_callable_referencing_pandas(dataframe):
     pdf, df = dataframe
@@ -1612,19 +1626,31 @@ def test_arrow_string_arrays():
     pd_s = pd.Series(["a", "b", "c"])
 
     cu_arr = xpd.arrays.ArrowStringArray._from_sequence(
-        cu_s, dtype=xpd.StringDtype("pyarrow")
+        cu_s, dtype=xpd.StringDtype(storage="pyarrow")
     )
     pd_arr = pd.arrays.ArrowStringArray._from_sequence(
-        pd_s, dtype=pd.StringDtype("pyarrow")
+        pd_s, dtype=pd.StringDtype(storage="pyarrow")
     )
 
     tm.assert_equal(cu_arr, pd_arr)
 
+    xpd_pa_np_storage_type = (
+        xpd.StringDtype("pyarrow_numpy")
+        if PANDAS_VERSION < version.parse("2.3.1")
+        else pd.StringDtype(storage="pyarrow", na_value=np.nan)
+    )
+
+    pd_pa_np_storage_type = (
+        pd.StringDtype("pyarrow_numpy")
+        if PANDAS_VERSION < version.parse("2.3.1")
+        else pd.StringDtype(storage="pyarrow", na_value=np.nan)
+    )
+
     cu_arr = xpd.core.arrays.string_arrow.ArrowStringArray._from_sequence(
-        cu_s, dtype=xpd.StringDtype("pyarrow_numpy")
+        cu_s, dtype=xpd_pa_np_storage_type
     )
     pd_arr = pd.core.arrays.string_arrow.ArrowStringArray._from_sequence(
-        pd_s, dtype=pd.StringDtype("pyarrow_numpy")
+        pd_s, dtype=pd_pa_np_storage_type
     )
 
     tm.assert_equal(cu_arr, pd_arr)
