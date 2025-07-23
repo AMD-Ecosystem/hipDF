3fae922d5c398378ee89e5b9995eb841af6dd061
Matthew Murray <41342305+Matt711@users.noreply.github.com>
2025-07-23 11:25:22 -0400

    Avoid using UVM on systems without a traditional memory resource (#19444)
    Closes https://github.com/rapidsai/cudf/issues/19427
    
    Authors:
      - Matthew Murray (https://github.com/Matt711)
      - Vyas Ramasubramani (https://github.com/vyasr)
    
    Approvers:
      - Richard (Rick) Zamora (https://github.com/rjzamora)
    
    URL: https://github.com/rapidsai/cudf/pull/19444

--- python/cudf_polars/cudf_polars/callback.py
+++ python/cudf_polars/cudf_polars/callback.py
@@ -24,7 +24,7 @@ from rmm._cuda import gpu
 
 from cudf_polars.dsl.tracing import CUDF_POLARS_NVTX_DOMAIN
 from cudf_polars.dsl.translate import Translator
-from cudf_polars.utils.config import _env_get_int
+from cudf_polars.utils.config import _env_get_int, get_total_device_memory
 from cudf_polars.utils.timer import Timer
 
 if TYPE_CHECKING:
