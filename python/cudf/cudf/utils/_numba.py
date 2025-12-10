6e251fb836f6c8052c43a4a79ac385f4d8d0ce30
Philipp Samfass <philipp.samfass@amd.com>
2025-12-10 09:59:42 -0500

    refactor: Remove CUDA-specific PTX file helpers
    Remove _get_best_ptx_file and CUDA-specific _get_ptx_file as upstream cuDF has also removed these. HIP equivalents are left intact for the time being.

--- python/cudf/cudf/utils/_numba.py
+++ python/cudf/cudf/utils/_numba.py
@@ -28,83 +28,7 @@ import numba
 from numba import config as numba_config
 from packaging import version
 
-
-def _get_best_ptx_file(archs, max_compute_capability):
-    """
-    Determine of the available PTX files which one is
-    the most recent up to and including the device compute capability.
-    """
-    filtered_archs = [x for x in archs if x[0] <= max_compute_capability]
-    if filtered_archs:
-        return max(filtered_archs, key=lambda x: x[0])
-    else:
-        return None
-
-
-def _get_ptx_file(path, prefix):
-    if "RAPIDS_NO_INITIALIZE" in os.environ:
-        # cc=70 ptx is always built
-        cc = int(os.environ.get("STRINGS_UDF_CC", "70"))
-    else:
-        from numba import cuda
-
-        dev = cuda.get_current_device()
-
-        # Load the highest compute capability file available that is less than
-        # the current device's.
-        cc = int(dev.amdgpu_arch.strip("gfx").rstrip(":sramecc+:xnack-"))
-    files = glob.glob(os.path.join(path, f"{prefix}*.ptx"))
-    if len(files) == 0:
-        raise RuntimeError(f"Missing PTX files for cc={cc}")
-    regular_sms = []
-
-    for f in files:
-        file_name = os.path.basename(f)
-        sm_number = file_name.rstrip(".ptx").lstrip(prefix)
-        if sm_number.endswith("a"):
-            processed_sm_number = int(sm_number.rstrip("a"))
-            if processed_sm_number == cc:
-                return f
-        else:
-            regular_sms.append((int(sm_number), f))
-
-    regular_result = None
-
-    if regular_sms:
-        regular_result = _get_best_ptx_file(regular_sms, cc)
-
-    if regular_result is None:
-        raise RuntimeError(
-            "This cuDF installation is missing the necessary PTX "
-            f"files that are <={cc}."
-        )
-    else:
-        return regular_result[1]
-
-
-def _setup_numba():
-    """
-    Configure the numba linker for use with cuDF. This consists of
-    potentially putting numba into enhanced compatibility mode
-    based on the user driver and runtime versions as well as the
-    version of the CUDA Toolkit used to build the PTX files shipped
-    with the user cuDF package.
-    """
-
-    # In CUDA 12+, pynvjitlink is used to provide minor version compatibility.
-    if find_spec("pynvjitlink") is not None:
-        # Assume CUDA 12+ if pynvjitlink is available and unconditionally
-        # enable pynvjitlink.
-        #
-        # If somehow pynvjitlink is available on a CUDA 11 installation, numba
-        # will fail with an error: "Enabling pynvjitlink requires CUDA 12."
-        # This is not a supported use case.
-        numba_config.CUDA_ENABLE_PYNVJITLINK = True
-
 # HIP implementation
-
-del _get_ptx_file
-
 
 def _get_ptx_file(path, prefix):
     """HIP implementation.
@@ -122,9 +46,6 @@ def _get_ptx_file(path, prefix):
             f"More than one LLVM file found for path '{path}' and prefix '{prefix}'."
         )
     return files[0]
-
-del _setup_numba
-
 
 def _setup_numba():
     """HIP implementation does nothing.
