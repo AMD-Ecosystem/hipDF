15dceee3543189dca1ad45884027c1db61f9901d
Philipp Samfass <philipp.samfass@amd.com>
2025-12-15 09:35:23 -0500

    fix/hipify: fix HIP compatibility
    - Fix CUDF_UNREACHABLE macro to work with HIP (use HIP_DEVICE_COMPILE
      guard)
    - Fix set_bit availability guard for __HIPCC__ path

--- cpp/include/cudf/column/column_device_view_base.cuh
+++ cpp/include/cudf/column/column_device_view_base.cuh
@@ -13,6 +13,29 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
+
+// MIT License
+//
+// Modifications Copyright (C) 2025 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #pragma once
 
 #include <cudf/detail/offsets_iterator.cuh>
@@ -662,7 +685,7 @@ class alignas(16) mutable_column_device_view_core : public detail::column_device
     return d_children[child_index];
   }
 
-#ifdef __CUDACC__  // because set_bit in bit.hpp is wrapped with __CUDACC__
+#if defined(__HIPCC__) || defined(__CUDACC__)  // because set_bit in bit.hpp is wrapped with __HIPCC__ or __CUDACC__
   /**
    * @brief Updates the null mask to indicate that the specified element is
    * valid
