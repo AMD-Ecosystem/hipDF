010fe2c010211c8cb6c4c4f87f1f873257c296f3
Philipp Samfass <philipp.samfass@amd.com>
2025-12-16 10:08:48 -0500

    fix: Remove no longer existing header include.

--- cpp/src/io/parquet/reader_impl_chunking.cu
+++ cpp/src/io/parquet/reader_impl_chunking.cu
@@ -38,7 +38,6 @@
 
 #include "compact_protocol_reader.hpp"
 #include "io/comp/gpuinflate.hpp"
-#include "io/comp/io_uncomp.hpp"
 #include "io/utilities/time_utils.cuh"
 #include "reader_impl.hpp"
 #include "reader_impl_chunking.hpp"
