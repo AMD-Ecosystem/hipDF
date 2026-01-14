a37ef0143126d7cd1f342f2c85a45435ff7dfcc0
Philipp Samfass <philipp.samfass@amd.com>
2026-01-14 10:42:38 +0000

    fix(interop): Use HIP-compatible funnelshift intrinsic in from_arrow_host
    Use __m_funnelshift_r instead of __funnelshift_r on HIP when warpsize is
    64, and fix shift parameter type to unsigned int as required by the intrinsic.
    Add missing cudf/detail/null_mask.cuh include for bitmask_bits constant.
    
    This fixes failing tests in INTEROP_TEST.

--- cpp/src/interop/from_arrow_host.cu
+++ cpp/src/interop/from_arrow_host.cu
@@ -14,6 +14,28 @@
  * limitations under the License.
  */
 
+// MIT License
+//
+// Modifications Copyright (C) 2026 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include "arrow_utilities.hpp"
 #include "from_arrow_host.hpp"
 
@@ -22,6 +44,7 @@
 #include <cudf/copying.hpp>
 #include <cudf/detail/interop.hpp>
 #include <cudf/detail/null_mask.hpp>
+#include <cudf/detail/null_mask.cuh>
 #include <cudf/detail/nvtx/ranges.hpp>
 #include <cudf/detail/utilities/grid_1d.cuh>
 #include <cudf/detail/utilities/integer_utils.hpp>
@@ -69,8 +92,12 @@ __device__ inline bitmask_type get_mask_word(bitmask_type const* __restrict__ so
   auto const curr_word        = source[word_index];
   auto const end_index        = (source_end_bit - 1) / bitmask_bits;
   auto const next_word        = (end_index > word_index) ? source[word_index + 1] : bitmask_type{0};
-  auto const shift            = static_cast<bitmask_type>(source_begin_bit % bitmask_bits);
+  auto const shift            = static_cast<unsigned int>(source_begin_bit % bitmask_bits);
+#ifndef CUDF_USE_WARPSIZE_32
+  return __m_funnelshift_r(curr_word, next_word, shift);
+#else
   return __funnelshift_r(curr_word, next_word, shift);
+#endif
 }
 
 /**
