65cb777b4bafee92bf8b2b01d614566f53a438a0
Philipp Samfass <philipp.samfass@amd.com>
2026-01-22 17:46:52 +0000

    Fix cuda_target import for HIP/AMD compatibility
    The cuda_target symbol is not exported in numba-hip, so we import hip_target
    and alias it as cuda_target instead. This allows string UDFs to work correctly
    on HIP/AMD platforms where numba.cuda.descriptor.cuda_target doesn't exist.

--- python/cudf/cudf/core/udf/strings_typing.py
+++ python/cudf/cudf/core/udf/strings_typing.py
@@ -1,5 +1,27 @@
 # Copyright (c) 2022-2025, NVIDIA CORPORATION.
 
+# MIT License
+#
+# Modifications Copyright (C) 2026 Advanced Micro Devices, Inc. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import operator
 
 import numpy as np
@@ -9,7 +31,8 @@ from numba.core.extending import models, register_model
 from numba.core.typing import signature as nb_signature
 from numba.core.typing.templates import AbstractTemplate, AttributeTemplate
 from numba.cuda.cudadecl import registry as cuda_decl_registry
-from numba.cuda.descriptor import cuda_target
+# FIXME(HIP/AMD): missing numba-hip mapping?
+from numba.cuda.descriptor import hip_target as cuda_target
 
 import rmm
 
