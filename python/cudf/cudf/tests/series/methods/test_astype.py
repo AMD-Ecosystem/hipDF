e71f0dae6c587f70cd61753902e5c26c9a919f97
Philipp Samfass <philipp.samfass@amd.com>
2026-01-27 14:35:28 +0000

    fix: Handle negative float to uint overflow in test_typecast_from_int_to_decimal
    On AMD backend, casting negative floats to unsigned integers results in large
    wrapped values (e.g., -112.23 -> 4294967184) that overflow when converting to
    low-precision decimals. Take absolute value for unsigned types to avoid this UB.
    
    Ported from commit 0bcb4ad9bc (test_decimal.py). Fixes test failures for
    uint64 with precision 9, 11, and 18.

--- python/cudf/cudf/tests/series/methods/test_astype.py
+++ python/cudf/cudf/tests/series/methods/test_astype.py
@@ -1,4 +1,27 @@
 # Copyright (c) 2025, NVIDIA CORPORATION.
+
+# MIT License
+#
+# Modifications Copyright (C) 2026 Advanced Micro Devices, Inc. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import datetime
 import zoneinfo
 from decimal import Decimal
@@ -1149,6 +1172,14 @@ def test_typecast_from_int_to_decimal(integer_types_as_str, precision, scale):
             np.nan,
         ]
     )
+    # NOTE(HIP/AMD): On AMD backend, cast from negative float to unsigned integer
+    # results in large unsigned integers (on NVIDIA they happen to be casted to 0).
+    # These large integers then do not fit into the Decimal64Dtypes used in this test.
+    # This is UB so we take the abs value here if an unsigned integer is used as
+    # from_dtype. See internal issue 106.
+    if integer_types_as_str.startswith("uint"):
+        data = data.abs()
+
     got = data.astype(integer_types_as_str)
 
     pa_arr = (
