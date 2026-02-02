d9d44da60db5c8b9b877d03b9d225665afdf0116
Philipp Samfass <philipp.samfass@amd.com>
2026-02-02 16:22:50 +0000

    Fix test_transform_udf for HIP/AMD port
    Add the required 'name' argument to compile_ptx_for_current_device.
    The cudf backend searches for this specific function name to identify
    and replace the UDF entry point in the generated code.

--- python/pylibcudf/tests/test_transform.py
+++ python/pylibcudf/tests/test_transform.py
@@ -1,5 +1,27 @@
 # Copyright (c) 2024-2025, NVIDIA CORPORATION.
 
+# MIT License
+#
+# Modifications Copyright (C) 2026 Advanced Micro Devices, Inc. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 import math
 
 import numba
@@ -84,8 +106,11 @@ def test_transform_udf():
     def op(a, b, c):
         return (a + b) * c
 
+    # NOTE(HIP/AMD): The name argument is required for the HIP/AMD port.
+    # The cudf backend searches for this name to identify the UDF in the code.
     ptx, _ = cuda.compile_ptx_for_current_device(
-        op, (numba.float64, numba.float64, numba.float64), device=True
+        op, (numba.float64, numba.float64, numba.float64), device=True,
+        name="udf_funcname_from_numba_to_be_replaced_in_libcudf"
     )
 
     A = 5.0
