rapidsai/cudf

Update to clang 20 (#18818)
Contributes to https://github.com/rapidsai/build-planning/issues/177

This PR updates the clang version to 20.

Authors:
  - Bradley Dice (https://github.com/bdice)

Approvers:
  - Vyas Ramasubramani (https://github.com/vyasr)
  - David Wendt (https://github.com/davidwendt)

URL: https://github.com/rapidsai/cudf/pull/18818

cpp/src/io/fst/lookup_tables.cuh
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022-2024, NVIDIA CORPORATION.
+ * Copyright (c) 2022-2025, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -499,11 +499,11 @@ class dfa_device_view {
   static constexpr int32_t MAX_TRANSLATED_OUT = TranslationTableT::MAX_TRANSLATED_OUT;
 
   using SymbolGroupStorageT      = std::conditional_t<is_complex_op<SymbolGroupIdLookupT>::value,
-                                                 typename SymbolGroupIdLookupT::TempStorage,
-                                                 typename cub::NullType>;
+                                                      typename SymbolGroupIdLookupT::TempStorage,
+                                                      typename cub::NullType>;
   using TransitionTableStorageT  = std::conditional_t<is_complex_op<TransitionTableT>::value,
-                                                     typename TransitionTableT::TempStorage,
-                                                     typename cub::NullType>;
+                                                      typename TransitionTableT::TempStorage,
+                                                      typename cub::NullType>;
   using TranslationTableStorageT = std::conditional_t<is_complex_op<TranslationTableT>::value,
                                                       typename TranslationTableT::TempStorage,
                                                       typename cub::NullType>;

AMD-Ecosystem/hipDF

cpp/src/io/fst/lookup_tables.cuh
@@ -1,5 +1,5 @@
 /*
- * Copyright (c) 2022-2024, NVIDIA CORPORATION.
+ * Copyright (c) 2022-2025, NVIDIA CORPORATION.
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -523,11 +523,11 @@ class dfa_device_view {
   static constexpr int32_t MAX_TRANSLATED_OUT = TranslationTableT::MAX_TRANSLATED_OUT;
 
   using SymbolGroupStorageT      = std::conditional_t<is_complex_op<SymbolGroupIdLookupT>::value,
-                                                 typename SymbolGroupIdLookupT::TempStorage,
-                                                 typename hipcub::NullType>;
+                                                      typename SymbolGroupIdLookupT::TempStorage,
+                                                      typename hipcub::NullType>;
   using TransitionTableStorageT  = std::conditional_t<is_complex_op<TransitionTableT>::value,
-                                                     typename TransitionTableT::TempStorage,
-                                                     typename hipcub::NullType>;
+                                                      typename TransitionTableT::TempStorage,
+                                                      typename hipcub::NullType>;
   using TranslationTableStorageT = std::conditional_t<is_complex_op<TranslationTableT>::value,
                                                       typename TranslationTableT::TempStorage,
                                                       typename hipcub::NullType>;