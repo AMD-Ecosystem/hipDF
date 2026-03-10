55c78c5c25ed49851f3e7a748d07555990a6c342
Philipp Samfass <philipp.samfass@amd.com>
2026-03-10 12:46:39 +0000

    fix: Resolve injected-class-name warning in table_to_array_tests
    Split chrono type construction to handle timestamps and durations
    separately, avoiding the ambiguous typename T::duration reference.
    Timestamps construct via T::duration{val}, durations directly from val.

--- cpp/tests/reshape/table_to_array_tests.cpp
+++ cpp/tests/reshape/table_to_array_tests.cpp
@@ -14,6 +14,28 @@
  * limitations under the License.
  */
 
+// MIT License
+//
+// Modifications Copyright (C) 2026 Advanced Micro Devices, Inc. All rights reserved.
+//
+// Permission is hereby granted, free of charge, to any person obtaining a copy
+// of this software and associated documentation files (the "Software"), to deal
+// in the Software without restriction, including without limitation the rights
+// to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+// copies of the Software, and to permit persons to whom the Software is
+// furnished to do so, subject to the following conditions:
+//
+// The above copyright notice and this permission notice shall be included in all
+// copies or substantial portions of the Software.
+//
+// THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+// IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+// FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+// AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+// LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+// OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+// SOFTWARE.
+
 #include <cudf_test/base_fixture.hpp>
 #include <cudf_test/column_wrapper.hpp>
 #include <cudf_test/type_list_utilities.hpp>
@@ -74,8 +96,10 @@ TYPED_TEST(TableToDeviceArrayTypedTest, SupportedTypes)
     std::vector<T> data(nrows);
     for (int row = 0; row < nrows; ++row) {
       auto val = col * nrows + row + 1;
-      if constexpr (cudf::is_chrono<T>()) {
+      if constexpr (cudf::is_timestamp<T>()) {
         data[row] = T(typename T::duration{val});
+      } else if constexpr (cudf::is_duration<T>()) {
+        data[row] = T{val};
       } else {
         data[row] = static_cast<T>(val);
       }
