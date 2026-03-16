a70f3915d2e1d130bb92139e73e04cf4b08cc0ba
randyh62 <randy.hartgrove@amd.com>
2026-03-16 12:30:17 -0700

    Update pandas.com/docs links

--- python/cudf/cudf/utils/ioutils.py
+++ python/cudf/cudf/utils/ioutils.py
@@ -1,4 +1,27 @@
 # Copyright (c) 2019-2025, NVIDIA CORPORATION.
+
+# MIT License
+#
+# Modifications Copyright (C) 2026 Advanced Micro Devices, Inc. All rights reserved.
+#
+# Permission is hereby granted, free of charge, to any person obtaining a copy
+# of this software and associated documentation files (the "Software"), to deal
+# in the Software without restriction, including without limitation the rights
+# to use, copy, modify, merge, publish, distribute, sublicense, and/or sell
+# copies of the Software, and to permit persons to whom the Software is
+# furnished to do so, subject to the following conditions:
+#
+# The above copyright notice and this permission notice shall be included in all
+# copies or substantial portions of the Software.
+#
+# THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND, EXPRESS OR
+# IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF MERCHANTABILITY,
+# FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT. IN NO EVENT SHALL THE
+# AUTHORS OR COPYRIGHT HOLDERS BE LIABLE FOR ANY CLAIM, DAMAGES OR OTHER
+# LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM,
+# OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE
+# SOFTWARE.
+
 from __future__ import annotations
 
 import datetime
@@ -706,7 +729,7 @@ chunksize : integer, default None
 
     Return JsonReader object for iteration.
     See the `line-delimited json docs
-    <http://pandas.pydata.org/pandas-docs/stable/io.html#io-jsonl>`_
+    <https://pandas.pydata.org/docs/user_guide/io.html#json>`_
     for more information on ``chunksize``.
     This can only be passed if `lines=True`.
     If this is None, the file will be read into memory all at once.
@@ -984,7 +1007,7 @@ In order to add another DataFrame or Series to an existing HDF file
 please use append mode and a different a key.
 
 For more information see the `user guide
-<https://pandas.pydata.org/pandas-docs/stable/user_guide/io.html#hdf5-pytables>`_.
+<https://pandas.pydata.org/docs/user_guide/io.html#hdf5-pytables>`_.
 
 Parameters
 ----------
